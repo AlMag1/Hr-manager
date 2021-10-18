ca7cfd70dfad05b9d8ae4a9077984a453886fa65
Aris Alexopoulos <alex.aris90@gmail.com>
2021-10-18 23:58:02 +0300

    Created simple landing page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Container from '@mui/material/Container';
 import ApplicationRouter from 'routes';
+import './styles/main.scss';
 
 const App = () => {
-  return <ApplicationRouter />;
+  return (
+    <Fragment>
+      <Container maxWidth="false" disableGutters className="app-container">
+        <ApplicationRouter />
+      </Container>
+    </Fragment>
+  );
 };
 
 export default App;
--- src/views/landing/index.js
+++ src/views/landing/index.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 const Landing = () => {
-  return <div>landing</div>;
+  return (
+    <Grid container className="landing-container">
+      <Grid item xs={6} className="landing-container__photo" />
+      <Grid
+        xs={6}
+        className="sign-in-container"
+        container
+        justifyContent="center"
+        direction="column"
+      >
+        <div className="sign-in-container__title">Sign in</div>
+        <TextField label="Username" className="sign-in-container__field" />
+        <TextField label="Password" type="password" />
+        <Button variant="contained" className="sign-in-container__button">
+          Sign In
+        </Button>
+        <Grid container className="sign-in-helpers">
+          <Grid item xs={6}>
+            <div className="sign-in-helpers__title">Forgot Password</div>
+          </Grid>
+          <Grid item xs={6}>
+            <div className="sign-in-helpers__title">
+              Don't have an account? Sign Up
+            </div>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Landing;
