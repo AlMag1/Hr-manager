f1a1aad04fbea9af4c94a48a1b300624d7110217
Aris Alexopoulos <alex.aris90@gmail.com>
2021-11-22 16:45:18 +0200

    Added custom font family

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Fragment } from 'react';
 import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import ApplicationRouter from 'routes';
 import './styles/main.scss';
 
+const theme = createTheme({
+  typography: {
+    fontFamily: `'Montserrat', sans-serif`,
+  },
+});
+
 const App = () => {
   return (
-    <Fragment>
+    <ThemeProvider theme={theme}>
       <Container maxWidth="false" disableGutters className="app-container">
         <ApplicationRouter />
       </Container>
-    </Fragment>
+    </ThemeProvider>
   );
 };
 
